36e5514175e33b8665f71e795bbcabe35b0acd4b
bhavna <bhavnasananse@gmail.com>
2021-05-15 07:42:19 -0700

    change to add new file

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-
-
-// 192.168.43.247
-export const environment = {
-  production: false,
-  ApiUrl: 'http://localhost:3000/'
-};
